38294b1a985927e2e0e3b75d90133ad357e9162d
Betty985 <1830265753@qq.com>
2024-01-18 15:36:00 +0800

    feat:数据持久化-SQLite数据库

--- app/src/main/java/com/example/broadcasttest/MainActivity.kt
+++ app/src/main/java/com/example/broadcasttest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.broadcasttest
 
 import android.content.BroadcastReceiver
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
@@ -26,6 +27,31 @@ class MainActivity : BaseActivity() {
         forceOffline.setOnClickListener {
             val intent = Intent("com.example.broadcasttest.FORCE_OFFLINE")
             sendBroadcast(intent)
+        }
+        val createDatabase =findViewById<Button>(R.id.createDatabase)
+        val dbHelper=MyDatabaseHelper(this,"BookStore.db",2)
+        createDatabase.setOnClickListener {
+            dbHelper.writableDatabase
+        }
+        val addData=findViewById<Button>(R.id.addData)
+        addData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            val values1 = ContentValues().apply {
+//                开始组装第一条数据
+                put("name", "The Da Vinci Code")
+                put("author", "Dan Brown")
+                put("pages", 454)
+                put("price", 16.96)
+            }
+            db.insert("Book", null, values1)
+            val values2 = ContentValues().apply {
+//                开始组装第二条数据
+                put("name", "The Lost Symbol")
+                put("author", "Dan Brown")
+                put("pages", 510)
+                put("price", 19.95)
+            }
+            db.insert("Book", null, values2)
         }
         timeChangeReceiver = TimeChangeReceiver()
         val intentFilter = IntentFilter()
